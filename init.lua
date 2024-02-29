aa06c03447571b0bc2e2e436a77c7994019aac73
DavidF <dfetzer@grayscale.io>
2024-02-29 01:35:35 -0500

    fancy text

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
 require("fetzy")
+
+require("onedarkpro").setup({
+  styles = {
+    types = "NONE",
+    methods = "NONE",
+    numbers = "NONE",
+    strings = "NONE",
+    comments = "italic",
+    keywords = "bold,italic",
+    constants = "NONE",
+    functions = "italic",
+    operators = "NONE",
+    variables = "NONE",
+    parameters = "NONE",
+    conditionals = "italic",
+    virtual_text = "NONE",
+  }
+})
+
 vim.cmd("colorscheme onedark_vivid")
 
