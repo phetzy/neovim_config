a3b2c49e12c2f4c6f7b6ae15e2d23109ec7ce2fe
Phetzy <111381691+phetzy@users.noreply.github.com>
2024-02-28 23:32:57 -0500

    font styling

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
 require("fetzy")
 vim.cmd("colorscheme onedark_vivid")
 
+require("onedarkpro").setup({
+  styles = {
+    types = "NONE",
+    methods = "NONE",
+    numbers = "NONE",
+    strings = "NONE",
+    comments = "italic",
+    keywords = "bold,italic",
+    constants = "NONE",
+    functions = "italic",
+    operators = "NONE",
+    variables = "NONE",
+    parameters = "NONE",
+    conditionals = "italic",
+    virtual_text = "NONE",
+  }
+})
