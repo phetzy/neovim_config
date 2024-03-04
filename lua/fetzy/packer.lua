caf43ba5ecef1f6ee1d061f3d5da4ed44e4c14ab
DavidF <dfetzer@grayscale.io>
2024-03-04 15:01:10 -0500

    added vim-be-good

--- lua/fetzy/packer.lua
+++ lua/fetzy/packer.lua
@@ -1,3 +1,4 @@
+require("fetzy.remap")
 
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
@@ -6,57 +7,59 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-}
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+	    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	    -- or                            , branch = '0.1.x',
+	    requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use ("olimorris/onedarkpro.nvim")
+    use ("olimorris/onedarkpro.nvim")
 
-  use ("nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'})
+    use ("nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'})
 
-  use ('nvim-treesitter/playground')
+    use ('nvim-treesitter/playground')
 
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = {'nvim-tree/nvim-web-devicons', opt = true }
-  }
+    use {
+	    'nvim-lualine/lualine.nvim',
+	    requires = {'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-  use ('theprimeagen/harpoon')
-  use ('mbbill/undotree')
-  use ('tpope/vim-fugitive')
+    use ('theprimeagen/harpoon')
+    use ('mbbill/undotree')
+    use ('tpope/vim-fugitive')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    use {
+	    'VonHeikemen/lsp-zero.nvim',
+	    branch = 'v3.x',
+	    requires = {
+		    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+		    {'williamboman/mason.nvim'},
+		    {'williamboman/mason-lspconfig.nvim'},
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lua'},
-		  {'rafamadriz/friendly-snippets'}
-	  }
-  }
+		    -- LSP Support
+		    {'neovim/nvim-lspconfig'},
+		    -- Autocompletion
+		    {'hrsh7th/nvim-cmp'},
+		    {'hrsh7th/cmp-nvim-lsp'},
+		    {'L3MON4D3/LuaSnip'},
+		    {'hrsh7th/cmp-buffer'},
+		    {'hrsh7th/cmp-path'},
+		    {'saadparwaiz1/cmp_luasnip'},
+		    {'hrsh7th/cmp-nvim-lua'},
+		    {'rafamadriz/friendly-snippets'}
+	    }
+    }
 
-  use {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-        require("nvim-autopairs").setup {}
-    end
-}
+    use {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    }
+
+    use {"ThePrimeagen/vim-be-good"}
 
 end)
