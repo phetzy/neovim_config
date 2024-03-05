8e93df52334ad08796238175a9c672f014f263ff
DavidF <dfetzer@grayscale.io>
2024-03-05 16:03:10 -0500

    color changes

--- init.lua
+++ init.lua
@@ -15,10 +15,15 @@ require("onedarkpro").setup({
     parameters = "NONE",
     conditionals = "italic",
     virtual_text = "NONE",
+  },
+  options = {
+      transparency = true
   }
 })
 
 vim.cmd("colorscheme onedark_vivid")
 
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "TelescopeNormal", {bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+vim.api.nvim_set_hl(0, "TelescopeBorder", {bg = "none"})
